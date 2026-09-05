85ca836c6230ae80b912c74980742bc44493897e
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2026-09-06 01:41:30 +0300

    Add exception handling classes

---
+++ src/main/java/org/example/librarymanagement/exception/DuplicateResourceException.java
@@ -0,0 +1,8 @@
+package org.example.librarymanagement.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}
