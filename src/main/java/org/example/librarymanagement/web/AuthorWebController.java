61c4540cff06734f929948a5e97e1e1f86f872ba
vataulorena <vatau.lorena@yahoo.com>
2026-09-06 15:41:11 +0300

    Add author create form with validation

--- src/main/java/org/example/librarymanagement/web/AuthorWebController.java
+++ src/main/java/org/example/librarymanagement/web/AuthorWebController.java
@@ -1,10 +1,15 @@
 package org.example.librarymanagement.web;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.example.librarymanagement.entity.Author;
 import org.example.librarymanagement.service.AuthorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +23,24 @@ public class AuthorWebController {
     public String listAuthors(Model model) {
         model.addAttribute("authors", authorService.getAll());
         return "authors/list";
+    }
+
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("author", new Author());
+        return "authors/form";
+    }
+
+    @PostMapping
+    public String createAuthor(
+            @Valid @ModelAttribute("author") Author author,
+            BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "authors/form";
+        }
+
+        authorService.create(author);
+        return "redirect:/authors";
     }
 }
