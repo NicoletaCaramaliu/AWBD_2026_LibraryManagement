5487896aee0e9639b88e3e46ff9a209eed46b0fd
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2026-09-07 00:50:16 +0300

    Enable admin initialization in production

--- src/main/java/org/example/librarymanagement/config/AdminInitializer.java
+++ src/main/java/org/example/librarymanagement/config/AdminInitializer.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("dev")
+@Profile({"dev","prodgit pul"})
 @RequiredArgsConstructor
 @Slf4j
 public class AdminInitializer implements CommandLineRunner {
