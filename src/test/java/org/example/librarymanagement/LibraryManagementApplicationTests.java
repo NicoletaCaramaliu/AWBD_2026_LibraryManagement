310818219d1c48bf3bfc8ee7d092d6abb2af25bf
vataulorena <vatau.lorena@yahoo.com>
2026-09-06 15:12:27 +0300

    Add JaCoCo test coverage configuration

--- src/test/java/org/example/librarymanagement/LibraryManagementApplicationTests.java
+++ src/test/java/org/example/librarymanagement/LibraryManagementApplicationTests.java
@@ -2,12 +2,13 @@ package org.example.librarymanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LibraryManagementApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
